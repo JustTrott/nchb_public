40a2491763969ee319996b86f34b2a4e86f8fb9f
JustTrott <greenvisp@gmail.com>
2024-07-07 14:33:50 +0500

    fix: buhgolts

--- src/services/standingsService.ts
+++ src/services/standingsService.ts
@@ -26,11 +26,12 @@ class StandingsService {
 	
 	async calculateBuhgolts(teamId: string): Promise<number> {
 		// 1. Find all battles for the given team
+		// populate teams and team.specificTeams for each team
 		const battles: IBattles[] = await Battles.find({ teams: teamId })
 		.populate('teams')
 		.populate('jury.specificTeams')
-		.populate('teams.specificTeams')
 		.exec();
+		console.log(battles.map(battle => battle.teams.map(team => team.specificTeams)));
 		// 2. Extract opponent from each battle, it can either be a team or jury
 		const opponents: IStandings[][] = battles.map(battle => {
 			if (battle.jury) {
@@ -38,19 +39,36 @@ class StandingsService {
 			}
 			return battle.teams.filter(team => team.id !== teamId);
 		});
-	
-		// 3. Calculate and return the total Buhgolts coefficient (sum of opponent totals). for each element in opponents find average of total
-		const buhgolts = opponents.map(opponent => {
-			const total = opponent.reduce((acc, team) => {
-				let opponentScore = team.total;
-				if (team.disqualified)
-				{
-					opponentScore = team.specificTeams.reduce((acc, t) => acc + t.total, 0) / team.specificTeams.length;
+		const buhgolts: number[] = [];
+		for (let i = 0; i < opponents.length; i++) {
+			let acc = 0;
+			for (let j = 0; j < opponents[i].length; j++) {
+				if (opponents[i][j].disqualified) {
+					// populate specificTeams for each team
+					const populatedTeam = await opponents[i][j].populate('specificTeams');
+					console.log(populatedTeam);
+					opponents[i][j].total = opponents[i][j].specificTeams.reduce((acc, t) => acc + t.total, 0) / opponents[i][j].specificTeams.length;
 				}
-				return acc + opponentScore;
-			}, 0);
-			return total / opponent.length;
-		});
+				acc += opponents[i][j].total;
+			}
+			buhgolts.push(acc / opponents[i].length);
+		}
+		
+		// 3. Calculate and return the total Buhgolts coefficient (sum of opponent totals). for each element in opponents find average of total
+		// const buhgolts = opponents.map(async opponent => {
+		// 	const total = opponent.reduce(async (acc, team) => {
+		// 		let opponentScore = team.total;
+		// 		if (team.disqualified)
+		// 			{
+		// 				// populate specificTeams for each team
+		// 				const populatedTeam = await team.populate('specificTeams');
+		// 				console.log(populatedTeam);
+		// 				opponentScore = team.specificTeams.reduce((acc, t) => acc + t.total, 0) / team.specificTeams.length;
+		// 		}
+		// 		return acc + opponentScore;
+		// 	}, 0);
+		// 	return total / opponent.length;
+		// });
 	
 		return buhgolts.reduce((acc, buhgolt) => acc + buhgolt, 0);
 	}
